798e81ded2d41f6a30198c27e9424197559b0813
Kristofor Carle <kristoforcarle@moabi.org>
2015-07-22 11:13:07 -0400

    include actual user names and UIDs in the data

--- services/query-ways.js
+++ services/query-ways.js
@@ -6,14 +6,26 @@ module.exports = function queryWays(knex, wayIds) {
 
   // helper to make raw queries, because knex version of these
   // simple selects was MUCH slower
+  function selectWithUserInfo(table, key, ids) {
+    if(ids.length === 0)
+      return Promise.resolve([]);
+    return knex.raw('select a.*, c.display_name as user, c.id as uid from '+table +
+      ' a left join changesets b on a.changeset_id = b.id' +
+      ' left join users c on b.user_id = c.id ' +
+     ' where a.'+key+' in ('+ ids.join(',') + ')')
+      .then(function (resp) {
+        return resp.rows;
+      });
+  }
+
   function select(table, key, ids) {
     if(ids.length === 0)
       return Promise.resolve([]);
     return knex.raw('select * from '+table +
-      ' where '+key+' in ('+ ids.join(',') + ')')
-      .then(function (resp) {
-        return resp.rows;
-      });
+        ' where '+key+' in ('+ ids.join(',') + ')')
+        .then(function (resp) {
+          return resp.rows;
+        });
   }
 
   // Query the desired ways and any way_nodes that are in those ways
@@ -23,7 +35,11 @@ module.exports = function queryWays(knex, wayIds) {
   // or other relations that are part of relations.
 
   return Promise.all([
-    knex('current_ways').whereIn('id', wayIds),
+    knex.select('current_ways.*', 'users.display_name as user', 'users.id as uid')
+        .from('current_ways')
+        .leftJoin('changesets', 'current_ways.changeset_id', 'changesets.id')
+        .leftJoin('users', 'changesets.user_id', 'users.id')
+        .whereIn('current_ways.id', wayIds),
     knex('current_way_nodes')
       .orderBy('sequence_id', 'asc')
       .whereIn('way_id', wayIds),
@@ -44,10 +60,10 @@ module.exports = function queryWays(knex, wayIds) {
     var relationIds = _.pluck(result[2], 'relation_id');
 
     return Promise.all(result.concat([
-      select('current_nodes', 'id', nodeIds),
+      selectWithUserInfo('current_nodes', 'id', nodeIds),
       select('current_way_tags', 'way_id', wayIds),
       select('current_node_tags', 'node_id', nodeIds),
-      select('current_relations', 'id', relationIds),
+      selectWithUserInfo('current_relations', 'id', relationIds),
       select('current_relation_tags', 'relation_id', relationIds)
     ]));
   })
@@ -84,7 +100,11 @@ module.exports = function queryWays(knex, wayIds) {
 
     return result;
 
-  });
+  })
+      .catch(function (err) {
+        console.log(err);
+        res(Boom.wrap(err));
+      });
 
 };
 
